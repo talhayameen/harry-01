71f676c8f63d692f99e885693864d2b9458baafa
talhayameen <talhasam48@gmail.com>
2023-06-21 20:18:15 +0500

    edit main

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void inputMethod() {
+    public static void inputMethodForAddition() {
 
         
         System.out.println("A basic addition program : " );
         //Making scanner class object here
         Scanner scan = new Scanner(System.in);
 
-        //Taking some input in int
+        //Taking some input in int/
         System.out.println("Enter number one");
         int num1 = scan.nextInt();
         System.out.println("Enter number two");
@@ -26,7 +26,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hi Exercise 1 java");
-        inputMethod();
+        inputMethodForAddition();
     }
 
 
