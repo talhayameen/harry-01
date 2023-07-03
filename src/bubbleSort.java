4a7f25857610e0d433af059290df5aaf95d6e402
talhayameen <talhasam48@gmail.com>
2023-07-03 14:53:10 +0500

    bubble sort

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -5,13 +5,16 @@ public class bubbleSort {
     int n = arr.length;
     int tmp = 0;
     for(int x=0; x < n; x++){
-     for(int y=1; y < (n-x); y++){
+
+     for(int y=1; y < (n-x); y++)
+     {
+
         if(arr[y-1] > arr[y]){
 
         //swap elements
         tmp = arr[y-1];
         arr[y-1] = arr[y];
-        
+
         arr[y] = tmp;
 
         }
