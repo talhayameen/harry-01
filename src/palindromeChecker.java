28c8433325b0b81e6418799f40200f9903c9186b
talhayameen <talhasam48@gmail.com>
2023-07-04 21:59:35 +0500

    test

--- src/palindromeChecker.java
+++ src/palindromeChecker.java
@@ -13,15 +13,13 @@ public class palindromeChecker {
     // loop wise checking both end of the string for palindrome
     // When checking if a string is a palindrome, we iterate 
     // through the characters from both ends and compare them. 
-    
     // The reason we divide the length of the string by 2 is to avoid redundant comparisons.
 
     //Let's consider an example to understand this. Suppose we have the string "racecar". 
     //If we compare the characters from both ends without dividing the length by 2, 
-    //we would perform the following comparisons: writting
+    //we would perform the following comparisons:
 
-
-
+    
 
     // Comparing the first character 'r' with the last character 'r'
     // Comparing the second character 'a' with the second-to-last character 'a'
