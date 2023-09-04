6c09c539dafd7f0539bda7424697137125d09594
talhayameen <talhasam48@gmail.com>
2023-09-04 19:45:38 +0500

    db con

--- src/databaseConnect.java
+++ src/databaseConnect.java
@@ -1,10 +1,5 @@
-import java.sql.*;
+
 
 public class databaseConnect {
-
-    private Connection connect = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
-    
+ 
 }
