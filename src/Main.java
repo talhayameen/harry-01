d3a3ed01a1d8418f865e3d91b2c78745e117641c
talhayameen <talhasam48@gmail.com>
2023-09-07 16:05:20 +0500

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-
-
+import banking.connection;
 
 public class Main {
 
--- src/banking/connection.java
+++ src/banking/connection.java
@@ -1,3 +1,4 @@
+package banking;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
