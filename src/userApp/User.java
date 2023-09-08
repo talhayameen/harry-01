2cb9ffd50cc2caf4295e5723e4d8521c29985a30
talhayameen <talhasam48@gmail.com>
2023-09-08 17:46:07 +0500

    msg

---
+++ src/userApp/User.java
@@ -0,0 +1,32 @@
+package userApp;
+
+public class User {
+
+    private String name;
+    private String fatherName;
+    private String contact;
+    private String email;
+
+    public String getName() {
+        return null;
+    }
+    public String getFatherName() {
+        return null;
+    }
+    public String getContact() {
+        return null;
+    }
+    public String getEmail() {
+        return null;
+    }
+    public void setName(String string) {
+        
+    }
+    public void setFatherName(String string) {
+    }
+    public void setContact(String string) {
+    }
+    public void setEmail(String string) {
+    }
+    
+}
