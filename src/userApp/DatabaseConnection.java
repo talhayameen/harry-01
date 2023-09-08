6dde9d807b1644051442a80036aefecbefec4a7b
talhayameen <talhasam48@gmail.com>
2023-09-08 19:43:08 +0500

    msg

--- src/userApp/DatabaseConnection.java
+++ src/userApp/DatabaseConnection.java
@@ -9,17 +9,24 @@ public class DatabaseConnection {
     private static Connection connection;
 
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                String url = "jdbc:mysql://localhost:3307/userapp";
-                String username = "root";
-                String password = "password";
-                connection = DriverManager.getConnection(url, username, password);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        // if (connection == null) {
+        //     try {
+        //         String url = "jdbc:mysql://localhost:3307/userapp";
+        //         String username = "root";
+        //         String password = "password";
+        //         connection = DriverManager.getConnection(url, username, password);
+        //     } catch (SQLException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+        // return connection;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-        return connection;
     }
     
 }
