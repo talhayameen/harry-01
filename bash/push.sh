f3cc1bddc84d8444105654248694d5e3aead480f
abdullahafzal30 <abdullahafzal305@gmail.com>
2023-11-02 15:03:15 +0500

    change by abdullah

--- bash/push.sh
+++ bash/push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Navigate to your project directory
-cd /home/bahl/Documents/devops-document/DevOps-Practice
+cd /home/bahl/Documents/DevOps-Practice
 
 # Add a space to the README file
-echo " " >> /home/bahl/Documents/devops-document/DevOps-Practice/README.md
+echo " " >> /home/bahl/Documents/DevOps-Practice/README.md
 
 # Stage and commit changes
 git add README.md
-git commit -m "Adding py"
+git commit -m "Adding py by abdullah"
 
 # Push changes to the repository
-git push origin main
+git push origin abdullah
